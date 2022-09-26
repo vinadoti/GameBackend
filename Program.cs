c910e7d5eac0b82ce0baeb03ed18b7d1c2030f27
Vivian Nadoti <vinadoti@gmail.com>
2022-09-26 14:12:28 -0300

    feat(swagger): Realiza ajustes e adiciona swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace GameBackend; // Definindo o namespace do projeto
 
-app.MapGet("/", () => "Hello World!");
+public class Program // Classe principal do projeto
+{
+    public static void Main(string[] args) // Método principal do projeto
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args); // Criando o builder do projeto
 
-app.Run();
+        // Definindo os serviços a serem usados no projeto
+        builder.Services.AddControllers(); // Adicionando o serviço de controllers
+        builder.Services.AddEndpointsApiExplorer(); // Adicionando o serviço de API Explorer
+        builder.Services.AddSwaggerGen(); // Adicionando o serviço de documentação Swagger
+
+        WebApplication app = builder.Build(); // Criando o projeto
+
+        // Adicionando o middleware de documentação Swagger ao projeto se estiver em desenvolvimento (IsDevelopment)
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        // Adicionando os controllers ao projeto
+        app.MapControllers(); 
+
+        app.Run(); // Executando o projeto
+    }
+}
