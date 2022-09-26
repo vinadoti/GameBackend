90ed0dc3541cc7aaf874fe9f26a6a8e12a51719b
Vivian Nadoti <vinadoti@gmail.com>
2022-09-26 14:18:05 -0300

    feat(Models): Adiciona o Models ao projeto

---
+++ Models/Game.cs
@@ -0,0 +1,10 @@
+namespace GameBackend.Models 
+{ 
+    public class Game // Classe Game
+    {
+        // Atributos da classe Game
+        public int gameId; 
+        public string name;
+        public bool status;
+    }
+}
