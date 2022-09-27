74c35b349b81b3e388a1c8c3a46460b1237fa519
Vivian Nadoti <vinadoti@gmail.com>
2022-09-26 22:29:40 -0300

    refactor(Controller/Models): Refatora código
    - Altera as propriedades da classe game e adiciona método Delete

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -11,27 +11,37 @@ namespace GameBackend.Controllers // Namespace do projeto
     // Recebe toda a requisição, processa e retorna mensagem ao usuário 
     public class GameController : ControllerBase // Define que a classe GameController herda de ControllerBase
     {
-        List<Game> lista = new List<Game>(); // Cria uma lista de objetos da classe Game
+        static List<Game> lista = new List<Game>(); // Cria uma lista de objetos da classe Game (static para ser acessada por todos os métodos)
 
         [HttpGet] // Define que o método Get é um método HTTP GET
         public ActionResult Read() // Método Get
         {
-            Game g1 = new Game(); // Instancia um objeto da classe Game
-            g1.gameId = 1; // Define o valor do atributo gameId
-            g1.name = "Game 1"; // Define o valor do atributo name
-            g1.status = true; // Define o valor do atributo status
-
-            lista.Add(g1); // Adiciona o objeto g1 na lista
-
             return Ok(lista); // Retorna a lista de objetos da classe Game - Resposta do método HTTP GET (200) - OK
         }
 
         [HttpPost] // Define que o método Post é um método HTTP POST
         public ActionResult Create(Game game) // Método Post
         {
-            // Processa a requisição e retorna mensagem ao usuário
+            game.GameId = Guid.NewGuid(); // Define o valor do atributo gameId
+            lista.Add(game); // Adiciona o objeto game na lista
 
-            return Created("", null); // Retorna o objeto game - Resposta do método HTTP POST (201) - Created
+            return Created(game.GameId.ToString(), game); // Retorna o valor do atributo gameId - Resposta do método HTTP POST (201) - Created
         }
+
+        [HttpDelete] // Define que o método Delete é um método HTTP DELETE
+        [Route("{id}")] // Define o endereco para acessar o método Delete
+        public ActionResult Delete(Guid id) // Método Delete
+        {
+            foreach (Game game in lista) // Percorre a lista de objetos da classe Game
+            {
+                if (game.GameId == id) // Verifica se o valor do atributo gameId do objeto game é igual ao valor do atributo id
+                {
+                    lista.Remove(game); // Remove o objeto game da lista
+                    return Ok(); // Retorna mensagem de sucesso - Resposta do método HTTP DELETE (200) - OK
+                }
+            }
+            return NotFound(); // Retorna mensagem de erro - Resposta do método HTTP DELETE (404) - Not Found
+        }
+
     }
 }
--- Models/Game.cs
+++ Models/Game.cs
@@ -3,8 +3,32 @@ namespace GameBackend.Models
     public class Game // Classe Game
     {
         // Atributos da classe Game
-        public int gameId; 
-        public string name;
-        public bool status;
+        private Guid gameId; // Id do jogo (Guid é um tipo de dado que representa um identificador único (uuid))  
+        private string name;
+        private bool status;
+
+        // propriedades da classe Game (Getters e Setters) 
+        public Guid GameId { 
+            get { return gameId; } // Retorna o valor do atributo gameId
+            set { gameId = value; } // Define o valor do atributo gameId
+        }
+
+        public string Name { 
+            get { return name; } // Retorna o valor do atributo name
+            set { name = value; } // Define o valor do atributo name
+        }
+
+        public bool Status { 
+            get { return status; } // Retorna o valor do atributo status
+            set { status = value; } // Define o valor do atributo status
+        }
+
+        // antes
+        // Game g1 = new Game(); // Instancia um objeto da classe Game
+        // g1.GameId = 1; // Define o valor do atributo gameId
+        // g1.Name = "Game 1"; // Define o valor do atributo name
+        // g1.Status = true; / Define o valor do atributo status 
+
+
     }
 }
